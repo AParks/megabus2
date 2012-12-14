066573559ec351b45728400fe0cea15f16c5a32b
A Parks <thalia1.618@gmail.com>
2012-12-14 18:00:48 -0500

    trip buses controller

--- app/controllers/trip_buses_controller.rb
+++ app/controllers/trip_buses_controller.rb
@@ -2,29 +2,21 @@ class TripBusesController < ApplicationController
   def index
 
     @trip = Trip.find(params[:id])
+
+    #@find(params[])
+
     @route = @trip.route.points
-    
-    #print @route[0]
-    #if @trip.leaving_from.name == @route[0]
-    #@route.each do |x|
+   
+    @buses = []
+    (0..@route.length).step(2).each do |x|
 
-    #    unless @trip.leaving_from == x
-    #     Bus.find_all_by_leaving_from x
-    #    end
-    #nd
+    leaving_from= Route.name_to_id[@route[x]]
+    traveling_to = Route.name_to_id[@route[x + 1]]
+      @buses = @buses + Bus.where("leaving_from_id =? AND traveling_to_id =?" , leaving_from, traveling_to)
+    end
 
-#join_condition = " ,categories where INNER JOIN categories ON categories.id=videos.category"
-
-
-
-    @buses = Bus.all(:joins => "JOIN trips ON buses.traveling_to_id = " + @trip.traveling_to_id.to_s +
-    " OR buses.leaving_from_id = " + @trip.leaving_from_id.to_s,
-    :select => 'DISTINCT buses.*',
-    :order=> 'leave_time asc'   )
 
     render "buses/index"
-    
-
 
   end
 
--- app/models/route.rb
+++ app/models/route.rb
@@ -11,6 +11,18 @@ class Route
 	@points
 	end
   
+  def self.name_to_id
+    { 
+      "Philadelphia,PA" => 1,
+      "Baltimore, MD" => 2,
+      "Boston, MA"=> 3,
+      "New York, NY" => 4,
+      "Pittsburgh, PA"   => 1,
+      "Cleveland, OH" => 4,
+      "Amherst, MA" => 7        
+    }
+  end
+  
   def self.multipoint_routes
     { 
       ["Pittsburgh, PA", "Baltimore, MD"]   => ["Pittsburgh, PA", "Philadelphia, PA", "Baltimore, MD"],
